bc7d754ef637e3e8c31115f27659ea35b7860d86
Jose Rodriguez <alberttora7@gmail.com>
2026-06-14 16:09:16 -0400

    format para los precios

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../helpers"
 
 
 type AmountDisplayProps = {
@@ -9,7 +10,7 @@ export default function AmountDisplay({label,amount}: AmountDisplayProps) {
   return (
    <p className="text-2xl text-blue-600 font-bold">
         {label}:{' '}
-        <span className="font-black text-black">{amount}</span>
+        <span className="font-black text-black">{formatCurrency(amount) }</span>
    </p>
   )
 }
