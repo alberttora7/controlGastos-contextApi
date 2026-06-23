5eeefb3372a3ed5d08c090a26432d6a2c5d5aa4f
Jose Rodriguez <alberttora7@gmail.com>
2026-06-23 13:10:00 -0400

    select the expense to update

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -6,7 +6,8 @@ export type BudgetActions =
 { type: 'show-modal'} |
 { type: 'close-modal'} |
 { type: 'add-expense', payload: {expense: DraftExpense }} |
-{ type: 'remove-expense', payload: {id: Expense['id'] }}
+{ type: 'remove-expense', payload: {id: Expense['id'] }} |
+{ type: 'get-expense-by-id', payload: {id: Expense['id']}}
 
 
 export type BudgetState = 
@@ -14,11 +15,14 @@ export type BudgetState =
     budget: number
     modal: boolean
     expenses: Expense[]
+    editingId: Expense['id']
 }
 export const initialState: BudgetState = {
     budget: 0,
     modal: false,
-    expenses: []
+    expenses: [],
+    editingId:  ''
+
 }
 
 const createExpense = (draftExpense: DraftExpense) : Expense => {
@@ -71,6 +75,14 @@ export const budgetReducer = (
             return{
                 ...state,
                 expenses: state.expenses.filter(expense => expense.id !==  action.payload.id)
+            }
+        }
+        
+        if(action.type === 'get-expense-by-id'){
+            return{
+                ...state,
+                editingId: action.payload.id,
+                modal: true
             }
         }
     return state
